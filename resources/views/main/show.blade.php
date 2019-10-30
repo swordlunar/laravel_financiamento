a807ac6c6c3564d96bb0da5cac5095b2c8da7454
swordlunar <danilorasengan2011@gmail.com>
2019-10-30 01:07:06 -0300

    Tradução 01

--- resources/views/main/show.blade.php
+++ resources/views/main/show.blade.php
@@ -4,6 +4,8 @@
     <div class="container">
         <div class="col-md-12">
             Você está visualizando o projeto: <strong> {{ $projeto->nome_projeto}}</strong>
+            <h3> Descrição do Projeto </h3>
+            {{ $projeto->descricao_projeto}}
         </div>
     </div>
 @endsection
