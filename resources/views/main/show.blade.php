d0ba6d959cdef14dcfcf08617ce92bfeb05da28c
swordlunar <danilorasengan2011@gmail.com>
2019-10-30 01:10:45 -0300

    Tradução 02

--- resources/views/main/show.blade.php
+++ resources/views/main/show.blade.php
@@ -3,9 +3,13 @@
 @section('content')
     <div class="container">
         <div class="col-md-12">
-            Você está visualizando o projeto: <strong> {{ $projeto->nome_projeto}}</strong>
-            <h3> Descrição do Projeto </h3>
-            {{ $projeto->descricao_projeto}}
+            <h3> Você está visualizando o projeto: {{ $projeto->nome_projeto}}</h3>
+            <h3> Descrição do Projeto: </h3>
+                {{ $projeto->descricao_projeto}}
+            <h3> Custo do Projeto: </h3>
+                {{ $projeto->custo_projeto}}
+            <h3> Tempo Para Desenvolvimento do Projeto: </h3>
+                {{ $projeto->tempo_de_desenvolvimento}}
         </div>
     </div>
 @endsection
