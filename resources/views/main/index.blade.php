25004ea8b8889deda90b5caa6a8a29238d3eb2df
Lucas Tavares Freire <lucas_tavares10@hotmail.com>
2019-11-17 20:22:10 -0300

    Modificações na tela de exibição de projetos

--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -3,6 +3,45 @@
 @section('content')
     <div class="container">
         <div class="col-md-12">
+
+            <table class="table">
+                <tbody>
+                  <tr class="row">
+                    @foreach($projetos as $projeto)
+
+                    @if ($projeto->id_criador != Auth::user()->id )
+                      <p></p>
+
+                    <td style="border-top:0px solid">
+
+                    <div class="card" style="width: 18rem;">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $projeto->nome_projeto }}</h5>
+                            <p class="card-text">
+                                custo do projeto: {{ $projeto->custo_projeto }}
+                                custo atual: {{ $projeto->custo_atual_projeto }}
+                            </p>
+                                <a href="{{ route('apoiadores_projeto.show', ['projeto' => $projeto->id_projeto]) }}">Apoiar</a>
+                        </div>
+                    </div>
+                        
+                    </td>
+                    @endif
+                    @endforeach
+                   
+                  </tr>
+                </tbody>
+              </table>
+        </div>
+    </div>
+
+@endsection
+
+<!-- 
+
+<div class="container">
+        <div class="col-md-12">
             
             <table class="table table-striped">
             <thead class="thead-light">
@@ -21,12 +60,12 @@
                     <td>{{ $projeto->nome_projeto }}</td>
                     <td>{{ $projeto->custo_projeto }}</td>
                     <td>{{ $projeto->custo_atual_projeto }}</td>
-                    <td>
-                            <!--<a href="{{ route('projeto.show', ['projeto' => $projeto->id_projeto]) }}">Visualizar</a>-->
-                            <a href="{{ route('apoiadores_projeto.show', ['projeto' => $projeto->id_projeto]) }}">Apoiar</a>
-                            <!--<a href="{{ route('projeto.edit', ['projeto' => $projeto->id_projeto]) }}">Editar</a>
-                            <a href="{{ route('projeto.destroy', ['projeto' => $projeto->id_projeto]) }}">Apagar</a>-->
-                    </td>
+                    <td> -->
+                                <!--<a href="{{ route('projeto.show', ['projeto' => $projeto->id_projeto]) }}">Visualizar</a>-->
+                            <!-- <a href="{{ route('apoiadores_projeto.show', ['projeto' => $projeto->id_projeto]) }}">Apoiar</a> -->
+                                <!--<a href="{{ route('projeto.edit', ['projeto' => $projeto->id_projeto]) }}">Editar</a>
+                                <a href="{{ route('projeto.destroy', ['projeto' => $projeto->id_projeto]) }}">Apagar</a>-->
+                    <!-- </td>
                     </tr>
                 @else
                     </tr>
@@ -35,6 +74,4 @@
             </tbody>
             </table>
         </div>
-    </div>
-
-@endsection
+    </div> -->
