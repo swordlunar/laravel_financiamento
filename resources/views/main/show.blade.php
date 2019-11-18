6e007abddb360f8d46a835150b205bf1f5c49440
Lucas Tavares Freire <lucas_tavares10@hotmail.com>
2019-11-17 22:00:03 -0300

    Visualização de Projetos

--- resources/views/main/show.blade.php
+++ resources/views/main/show.blade.php
@@ -3,21 +3,27 @@
 @section('content')
     <div class="container">
         <div class="col-md-12">
-            <h3> Você está visualizando o projeto: {{ $projeto->nome_projeto}}</h3>
-            <h3> Descrição do Projeto: </h3>
-                {{ $projeto->descricao_projeto}}
-            <h3> Custo do Projeto: </h3>
-                {{ $projeto->custo_projeto}}
-            <h3> Tempo Para Desenvolvimento do Projeto: </h3>
-                {{ $projeto->tempo_de_desenvolvimento}}
-                <figure>
-                    <img src='{{$projeto->foto1_projeto }}' alt="Minha Figura">	
-                    <figcaption>Informações da Figura</figcaption>
-                </figure>
-                <figure>
-                    <img src='{{$projeto->foto2_projeto }}' alt="Minha Figura">	
-                    <figcaption>Informações da Figura</figcaption>
-                </figure>
+            <h2 style="text-align:center; margin-top:4%">{{ $projeto->nome_projeto}}</h2>
+            <hr>
+            <p><h4>Descrição:</h4> {{ $projeto->descricao_projeto}}</p>
+            <p> <h4>Custo do Projeto:</h4> {{ $projeto->custo_projeto}}</p>
+            <p> <h4>Tempo Para Desenvolvimento do Projeto:</h4> {{ $projeto->tempo_de_desenvolvimento}}</p>
+            <p> <h4>Imagens:</h4><br>
+                <div class="row">
+                    <div class="col-md-6">                
+                        <figure>
+                            <img src='{{$projeto->foto1_projeto }}' alt="Minha Figura" style="max-width: 500px">	
+                            <!-- <figcaption>Informações da Figura</figcaption> -->
+                        </figure>
+                    </div>
+                    <div class="col-md-6">
+                        <figure>
+                            <img src='{{$projeto->foto2_projeto }}' alt="Minha Figura"  style="max-width: 500px">	
+                            <!-- <figcaption>Informações da Figura</figcaption> -->
+                        </figure>
+                    </div>
+                </div>
+            </p>
         </div>
     </div>
 @endsection
